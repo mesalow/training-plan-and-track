b18c06478fc4cd6701d86023a62ded35fde24c76
Michael <michaelsalow@web.de>
2021-08-30 19:40:35 +0200

    migrate: first tables

---
+++ migrations/001-initial.sql
@@ -0,0 +1,28 @@
+--------------------------------------------------------------------------------
+-- Up
+--------------------------------------------------------------------------------
+
+CREATE TABLE IF NOT EXISTS exercise (
+    ex_id INTEGER PRIMARY KEY,
+    ex_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS muscle_group (
+    musc_id INTEGER PRIMARY KEY,
+    musc_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ex_msc_map (
+    ex_ex_id INTEGER,
+    musc_musc_id INTEGER,
+    FOREIGN KEY (ex_ex_id) REFERENCES exercise(ex_id),
+    FOREIGN KEY (musc_musc_id) REFERENCES muscle_group(musc_id)
+);
+
+--------------------------------------------------------------------------------
+-- Down
+--------------------------------------------------------------------------------
+
+DROP TABLE exercise;
+DROP TABLE muscle_group;
+DROP TABLE ex_msc_map;
