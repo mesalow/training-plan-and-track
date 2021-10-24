5fe884957075f501bfcc67078fa753a5d41dbdae
Michael <michaelsalow@web.de>
2021-10-24 16:14:16 +0200

    fix: correct import

--- client/src/views/CreatePlan/ExerciseInput.vue
+++ client/src/views/CreatePlan/ExerciseInput.vue
@@ -13,7 +13,7 @@
 </template>
 <script lang="ts">
 import { ref, watch } from 'vue';
-import { useState, State } from '../../store/state';
+import { useState, State } from '../../store/createPlanState';
 
 export default {
   props: {
