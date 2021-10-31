7bc9420b30f12e3a8ed920db057b4cd2a44c4dd5
Michael <michaelsalow@web.de>
2021-10-31 11:52:15 +0100

    refact: some refactorings and typings

--- server/src/Domain/Progression.ts
+++ server/src/Domain/Progression.ts
@@ -5,78 +5,48 @@ import { ExerciseDTO } from "../Infrastructure/PlanRepository";
 import { ActualSetDTO } from "../Infrastructure/TrainingRepository";
 
 interface PlannedDayDTO {
-    dayNumber: number,
-    exercises: ExerciseDTO[],
+  dayNumber: number;
+  exercises: ExerciseDTO[];
 }
 
 interface ActualDay {
-    dayNumber: number;
-    exercises: ActualExercise[];
-  }
-  
-  interface ActualExercise {
-    name: string;
-    progression: progression;
-    tm: number | null;
-    id: number;
-    sets: MaybeDoneSet[];
-  }
-  type progression = "T1" | "T2a" | "T2b" | "T3";
-  type MaybeDoneSet = NotDoneYetSet | ActualSet;
-  
-  interface NotDoneYetSet {
-    weight: null;
-    reps: null;
-    expected: ExpectedSet;
-  }
-  
-  export interface ActualSet {
-    weight: number;
-    reps: number;
-    expected: ExpectedSet;
-  }
-export const getProgress = (completePlan: PlannedDayDTO[], allActualSets: ActualSetDTO[]): { weekNumber: number, days: ActualDay[] }[] => {
-  const trainingModel = new JackednTan2();
+  dayNumber: number;
+  exercises: ActualExercise[];
+}
+
+interface ActualExercise {
+  name: string;
+  progression: progression;
+  tm: number | null;
+  id: number;
+  sets: MaybeDoneSet[];
+}
+type progression = "T1" | "T2a" | "T2b" | "T3";
+type MaybeDoneSet = NotDoneYetSet | ActualSet;
+
+interface NotDoneYetSet {
+  weight: null;
+  reps: null;
+  expected: ExpectedSet;
+}
+
+export interface ActualSet {
+  weight: number;
+  reps: number;
+  expected: ExpectedSet;
+}
+const trainingModel = new JackednTan2();
+
+export const getProgress = (
+  completePlan: PlannedDayDTO[],
+  allActualSets: ActualSetDTO[]
+): { weekNumber: number; days: ActualDay[] }[] => {
   const weeks = [1, 2, 3, 4, 5, 6];
   return weeks.map((weekNumber) => {
     const weekExercises = completePlan.map((day) => {
       const { dayNumber, exercises } = day;
-      const expectedSetsForDay = exercises.map((ex) => {
-        const actualSetsForExercise = allActualSets.filter((actualSet) => {
-          return (
-            actualSet.week === weekNumber &&
-            actualSet.day === dayNumber &&
-            actualSet.ex_ex_id === ex.id
-          );
-        });
-        debug(
-          "TrainingController, actualSets found: %o",
-          actualSetsForExercise
-        );
-        const expectedSets = trainingModel.getExpectedSets(
-          weekNumber,
-          ex.progression as progression,
-          ex.tm
-        );
-        const sets = expectedSets.map((expectedSet, idx) => {
-          let weight = null;
-          let reps = null;
-          const actualSet = actualSetsForExercise[idx];
-          if (actualSet) {
-            weight = actualSet.weight;
-            reps = actualSet.reps;
-          }
-          return {
-            expected: expectedSet,
-            weight,
-            reps,
-          };
-        });
-        return {
-          ...ex,
-          sets,
-        };
-      });
+      const getActualExercise = getActualExerciseFactory(allActualSets)(weekNumber)(dayNumber);
+      const expectedSetsForDay = exercises.map((ex) => getActualExercise(ex));
       return {
         dayNumber,
         exercises: expectedSetsForDay as ActualExercise[],
@@ -88,3 +58,38 @@ export const getProgress = (completePlan: PlannedDayDTO[], allActualSets: Actual
     };
   });
 };
+
+const getActualExerciseFactory =
+  (allActualSets) => (weekNumber) => (dayNumber) => (ex: ExerciseDTO) => {
+    const actualSetsForExercise = allActualSets.filter((actualSet) => {
+      return (
+        actualSet.week === weekNumber &&
+        actualSet.day === dayNumber &&
+        actualSet.ex_ex_id === ex.id
+      );
+    });
+    debug("TrainingController, actualSets found: %o", actualSetsForExercise);
+    const expectedSets = trainingModel.getExpectedSets(
+      weekNumber,
+      ex.progression as progression,
+      ex.tm
+    );
+    const sets = expectedSets.map((expectedSet, idx) => {
+      let weight = null;
+      let reps = null;
+      const actualSet = actualSetsForExercise.find(actualSet => actualSet.set_number === idx+1);;
+      if (actualSet) {
+        weight = actualSet.weight;
+        reps = actualSet.reps;
+      }
+      return {
+        expected: expectedSet,
+        weight,
+        reps,
+      };
+    });
+    return {
+      ...ex,
+      sets,
+    };
+  };
