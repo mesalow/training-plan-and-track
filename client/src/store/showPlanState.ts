a9b122481354492591e444e3fc3a77afed397a9e
Michael <michaelsalow@web.de>
2021-11-21 16:12:32 +0100

    feat: logic to automatically update set1-Sets

--- client/src/store/showPlanState.ts
+++ client/src/store/showPlanState.ts
@@ -1,8 +1,5 @@
-import {
-  reactive,
-  provide,
-  inject,
-} from 'vue';
+import { reactive, provide, inject } from 'vue';
+import { getProgress } from '@/api';
 
 export interface Plan {
   planId: number;
@@ -10,10 +7,46 @@ export interface Plan {
   program: string;
 }
 
-const allPlans:Plan[] = [];
-
+const allPlans: Plan[] = [];
 export interface ShowPlanState {
   allPlans: Plan[];
+  currentPlanId: number;
+  currentPlan: any;
+  load: (state: ShowPlanState) => any;
+}
+
+const updateSet1 = (exercise: any) => {
+  const rExercise = exercise;
+  if (!exercise.sets[0]) {
+    console.log('warning: %o', exercise);
+  }
+  if (!exercise.sets[0].weight) {
+    return exercise;
+  }
+  rExercise.sets = exercise.sets.map((set: any) => {
+    const rSet = set;
+    if (set.expected.weight === 'Set1') {
+      rSet.expected.weight = exercise.sets[0].weight;
+    }
+    return set;
+  });
+  return rExercise;
+};
+
+const updatePlan = (plan: any) => plan.map((week: any) => {
+  const rweek = week;
+  rweek.days = week.days.map((day: any) => {
+    const rday = day;
+    rday.exercises = day.exercises.map((exercise: any) => {
+      if (exercise.sets.length && (exercise.progression === 'T3' || exercise.progression === 'T2b')) {
+        return updateSet1(exercise);
+      }
+      return exercise;
+    });
+    return rday;
+  });
+  return rweek;
+});
 
 export const load = async (state: ShowPlanState) => {
   try {
@@ -24,7 +57,7 @@ export const load = async (state: ShowPlanState) => {
     console.log('loaded current plan %o', state.currentPlan);
   } catch (error) {
     console.error('showPlans.beforeMount error', error);
-}
+  }
 };
 
 export const stateSymbol = Symbol('showplanstate');
@@ -32,6 +65,7 @@ export const createState = () => reactive({
   allPlans,
   currentPlanId: null,
   currentPlan: null,
+  load,
 });
 export const useState = () => inject(stateSymbol);
 export const provideState = () => provide(stateSymbol, createState());
